c26c7ed841e55dbb0ce86e72c865ebb530974dd2
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-06-18 14:03:09 +0300

    1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainerEl = document.querySelector(".gallery");
+galleryContainerEl.style.listStyle = 'none';
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>`;
+  })
+  .join("");
+
+galleryContainerEl.insertAdjacentHTML("beforeend", markup);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
