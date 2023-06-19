a4982e16df7e142cb4f50405b164a50f4c97fa3d
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-06-20 00:13:50 +0300

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const feedbackStateKey = 'feedback-form-state';
+
+const saveFormState = throttle(() => {
+  const feedbackState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(feedbackStateKey, JSON.stringify(feedbackState));
+}, 500);
+
+function fillFormFromState() {
+  const feedbackState = JSON.parse(localStorage.getItem(feedbackStateKey));
+  if (feedbackState) {
+    emailInput.value = feedbackState.email;
+    messageInput.value = feedbackState.message;
+  } else {
+    emailInput.value = '';
+    messageInput.value = '';
+  }
+}
+
+fillFormFromState();
+
+form.addEventListener('input', saveFormState);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const feedbackState = {
+    email: '',
+    message: '',
+  };
+  localStorage.setItem(feedbackStateKey, JSON.stringify(feedbackState));
+  console.log(feedbackState);
+  emailInput.value = '';
+  messageInput.value = '';
+});
